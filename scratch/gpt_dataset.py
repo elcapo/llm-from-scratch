fed945cef824e7a02919aecb5a0eb65f57146c51
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2024-11-11 18:15:03 +0000

    optimize memory usage in the dataset

--- scratch/gpt_dataset.py
+++ scratch/gpt_dataset.py
@@ -4,15 +4,18 @@ from .tokenizers.base_tokenizer import BaseTokenizer
 
 class GptDataset(Dataset):
     def __init__(self, text: str, tokenizer: BaseTokenizer, max_length: int = 1024, stride: int = 1):
-        self.ids = []
-
-        token_ids = tokenizer.encode(text)
-        for i in range(0, len(token_ids) - max_length + 1, stride):
-            chunk = token_ids[i: i + max_length + 1]
-            self.ids.append(tensor(chunk))
+        self.token_ids = tokenizer.encode(text)
+        self.max_length = max_length
+        self.stride = stride
     
     def __len__(self):
-        return len(self.ids) - 1
+        return (len(self.token_ids) - self.max_length) // self.stride
     
     def __getitem__(self, index):
-        return self.ids[index], self.ids[index + 1]
+        if index + self.max_length + 1 >= len(self.token_ids):
+            raise IndexError('list index out of range')
+        step = index * self.stride
+        return (
+            self.token_ids[step : step + self.max_length],
+            self.token_ids[step + 1 : step + self.max_length + 1]
+        )
