7eb8a165583a54d2bc498fb15a1b13d1ec2ebe4e
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2024-11-11 17:48:52 +0000

    reduce memory usage by storing less lists

--- scratch/gpt_dataset.py
+++ scratch/gpt_dataset.py
@@ -4,18 +4,15 @@ from .tokenizers.base_tokenizer import BaseTokenizer
 
 class GptDataset(Dataset):
     def __init__(self, text: str, tokenizer: BaseTokenizer, max_length: int = 1024, stride: int = 1):
-        self.input_ids = []
-        self.target_ids = []
+        self.ids = []
 
         token_ids = tokenizer.encode(text)
-        for i in range(0, len(token_ids) - max_length, stride):
-            input_chunk = token_ids[i: i + max_length]
-            target_chunk = token_ids[i + 1: i + max_length + 1]
-            self.input_ids.append(tensor(input_chunk))
-            self.target_ids.append(tensor(target_chunk))
+        for i in range(0, len(token_ids) - max_length + 1, stride):
+            chunk = token_ids[i: i + max_length + 1]
+            self.ids.append(tensor(chunk))
     
     def __len__(self):
-        return len(self.input_ids)
+        return len(self.ids) - 1
     
     def __getitem__(self, index):
-        return self.input_ids[index], self.target_ids[index]
+        return self.ids[index], self.ids[index + 1]
