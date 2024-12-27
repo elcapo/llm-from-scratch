ddf15f0897c32bbf229849e680717e2560810584
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2024-12-27 12:59:24 +0000

    adjust the simple tokenizer tests

--- tests/tokenizers/test_simple_tokenizer.py
+++ tests/tokenizers/test_simple_tokenizer.py
@@ -11,7 +11,7 @@ def test_simple_tokenizer_encode(the_veredict):
     text = "Hello, do you like tea? <|endoftext|> In the sunlit terraces of the palace."
     tokens = tokenizer.encode(text)
     # Assert
-    assert tokens == [1131, 5, 355, 1126, 628, 975, 10, 1130, 55, 988, 956, 984, 722, 988, 1131, 7]
+    assert tokens == [11, 5, 357, 1128, 630, 977, 12, 10, 57, 990, 958, 986, 724, 990, 11, 7]
 
 def test_simple_tokenizer_decode(the_veredict):
     # Prepare
